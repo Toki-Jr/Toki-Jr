8f7988bcb661b27199c4925cf34683dffbbd5586
Toki-Jr <robertrandrianandrasana62@gmail.com>
2026-07-16 23:32:58 +0300

    feat: improve code

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,5 +1,5 @@
-// import * as React from "react"
-
+/* eslint-disable react-refresh/only-export-components */
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
 function Card({
--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -1,6 +1,6 @@
-// import * as React from "react"
+/* eslint-disable react-refresh/only-export-components */
+import * as React from "react"
 import { Dialog as DialogPrimitive } from "radix-ui"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { XIcon } from "lucide-react"
